55b2263759c73835842fa2e8a6dc5e20c656c7f4
Codey Pinero <codey.pinero@gmail.com>
2019-03-13 00:35:21 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, user_input, current_player = "X")
-  board[user_input] = current_player
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
